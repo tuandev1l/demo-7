108ed70b17c84b8f3d017f9066b062b9a23d3d33
tuandev1l <tuanprogrammer001@gmail.com>
2023-02-15 03:56:25 +0700

    2

--- src/components/MainContent.test.jsx
+++ src/components/MainContent.test.jsx
@@ -4,9 +4,6 @@ import userEvent from '@testing-library/user-event';
 
 import MainContent from './MainContent';
 
-// My tests!
-// Add yet another comment
-
 describe('MainContent', () => {
   it('should render a button', () => {
     render(<MainContent />);
